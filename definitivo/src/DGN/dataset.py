e510202a5e46cb137a9ab3c49b944acbde25d74e
lorebenucci <lore.benucci@gmail.com>
2026-02-08 10:58:25 +0100

    more refining

--- definitivo/src/DGN/dataset.py
+++ definitivo/src/DGN/dataset.py
@@ -12,16 +12,14 @@ from tqdm.auto import tqdm
 
 
 def one_hot_encoding(x,list_properties):
-    # Se l'elemento 'x' non è nella lista consentita, usiamo l'ultimo elemento (spesso 'Unknown')
     if x not in list_properties:
         x = list_properties[-1]
-    
-    # Crea una lista: metti 1 se l'elemento corrisponde a x, altrimenti 0
     return [1 if s == x else 0 for s in list_properties]
     
 class Dataset_tox21(Dataset):
     
     def __init__(self,smiles,labels,global_parameters,permitted_atoms,hybrization_type,degree_atoms,number_hydrogens,permitted_bonds,stereo_list):
+        
         self.smiles=smiles
         self.labels=labels
         self.permitted_atoms=permitted_atoms
@@ -30,23 +28,22 @@ class Dataset_tox21(Dataset):
         self.number_hydrogens=number_hydrogens
         self.permitted_bonds=permitted_bonds
         self.stereo_list=stereo_list
-        #definition Chemical rules for acceptor and donators
+        
+        # Chemical rules for acceptor and donators
         self.fdefName = os.path.join(RDConfig.RDDataDir, 'BaseFeatures.fdef')
         self.factory = ChemicalFeatures.BuildFeatureFactory(self.fdefName)
-        
-        
         self.data_list = []
-        
         scaler = StandardScaler()
         norm_global_feats = scaler.fit_transform(np.array(global_parameters))
         
-        # Process from SMILES->GRAPH
+        # converting all molecules
         for i in tqdm(range(len(smiles))):
             data = self.conversion_to_graph(smiles[i], labels[i], torch.tensor([norm_global_feats[i]], dtype=torch.float))
             if data is not None:
                 self.data_list.append(data)
         
     
+    # extract atom (node) features
     def get_atom_features(self,atom,donor_indices,acceptor_indices):
         
         atom_feature=[]
@@ -61,19 +58,16 @@ class Dataset_tox21(Dataset):
         atom_feature += [1 if atom_idx in donor_indices else 0] # h-bond donator
         atom_feature += [1 if atom_idx in acceptor_indices else 0] # h-bond acceptor
         
-        # 9. Chirality (Tag R/S/None)
+        # chirality
         try:
             chirality = atom.GetProp('_CIPCode')
-            if chirality == 'R':
-                atom_feature += [1, 0, 0] # R
-            elif chirality == 'S':
-                atom_feature += [0, 1, 0] # S
-            else:
-                atom_feature += [0, 0, 1] # none
+            if chirality == 'R': atom_feature += [1, 0, 0] # R
+            elif chirality == 'S': atom_feature += [0, 1, 0] # S
+            else: atom_feature += [0, 0, 1] # none
         except:
             atom_feature += [0, 0, 1] # Any defined (none)
         
-        #formal charge (-2,-1,0,+1,+2)   
+        # formal charge (-2, -1, 0, +1, +2)   
         formal_charge = atom.GetFormalCharge()
         charges_list = [-2, -1, 0, 1, 2]
         atom_feature += one_hot_encoding(formal_charge, charges_list)
@@ -81,41 +75,36 @@ class Dataset_tox21(Dataset):
         return torch.tensor(atom_feature, dtype=torch.float)
         
     
+    # extract bond (edge) features
     def get_bond_features(self,bond):
         
         bond_feature = []
         
-        #extract type of bond
-        bond_type = bond.GetBondType()
+        bond_type = bond.GetBondType() #extract type of bond (SINGLE, DOUBLE, TRIPLE, AROMATIC)
         bond_feature += one_hot_encoding(bond_type, self.permitted_bonds)
-    
-        # 2. Is Conjugated (Boolean)
-        bond_feature += [1 if bond.GetIsConjugated() else 0]
-    
-        # 3. Is In Ring (Boolean)
-        bond_feature += [1 if bond.IsInRing() else 0]
-    
-        # 4. Stereochimica (Optional: None, E, Z, etc.)
-        stereo = bond.GetStereo()
-    
+        bond_feature += [1 if bond.GetIsConjugated() else 0] # is Conjugated? (Boolean)
+        bond_feature += [1 if bond.IsInRing() else 0] # is in ring ? (Boolean)
+        stereo = bond.GetStereo() # stereochemistry
         bond_feature += one_hot_encoding(stereo, self.stereo_list)
 
         return torch.tensor(bond_feature, dtype=torch.float)
         
+    
+    
+    # SMILES to graph
     def conversion_to_graph(self,smiles,labels,global_features): 
         
-        #transformation from smile to graph representation
+
+        # ATOMS (nodes)
         mol = Chem.MolFromSmiles(smiles)
         if mol is None:
             return None
         
-        #Compute Donators and Acceptors
         feats = self.factory.GetFeaturesForMol(mol)
         donor_indices = []
         acceptor_indices = []
         
         for feat in feats:
-            #selection of feats which belong to Donor or Acceptor
             if feat.GetFamily() == 'Donor':
                 donor_indices.extend(feat.GetAtomIds())
             elif feat.GetFamily() == 'Acceptor':
@@ -123,27 +112,21 @@ class Dataset_tox21(Dataset):
             
         donor_indices = set(donor_indices)
         acceptor_indices = set(acceptor_indices)
-        
-        
         atom_features_list = []
-        #Assing chirality
+    
         Chem.AssignStereochemistry(mol)
         for atom in mol.GetAtoms():
-            #obtain for each atom==node a set of features
             atom_features_list.append(self.get_atom_features(atom, donor_indices, acceptor_indices))
         
-        #stack in x all features
-        x = torch.stack(atom_features_list)
+        x = torch.stack(atom_features_list) # atom features vector
         
-        #Computation of arcs 
+        # BONDS (edges) 
         edge_indices = []
         edge_features_list=[]
         for bond in mol.GetBonds():
             k = bond.GetBeginAtomIdx()
             j = bond.GetEndAtomIdx()
             edge_indices += [[k, j], [j, k]]
-            
-            #bond features
             bond_feat = self.get_bond_features(bond) 
             edge_features_list += [bond_feat, bond_feat]
     
@@ -154,7 +137,6 @@ class Dataset_tox21(Dataset):
             edge_index = torch.tensor(edge_indices, dtype=torch.long).t().contiguous()
             edge_attr = torch.stack(edge_features_list)
 
-        #labels 
         y_tensor = None
         if labels is not None:
             y_tensor = torch.tensor(labels, dtype=torch.float).view(1, -1)
